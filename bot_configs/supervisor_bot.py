91e5481e70a074bfce202951efe74f9e502d3c63
Gosha Lubashev <bluecapacitor@gmail.com>
2024-11-02 23:11:00 -0700

    Fixed remote downgrading bug 2

--- bot_configs/supervisor_bot.py
+++ bot_configs/supervisor_bot.py
@@ -374,7 +374,7 @@ async def update_further(context: UpdateHandlerContext):
         reply_to_message_id=query_message_id
     )
     proc: subprocess.Process = await create_subprocess_shell(
-        f"git pull main; git checkout main",
+        f"git pull origin main; git checkout main",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
@@ -433,7 +433,7 @@ async def update_further(context: UpdateHandlerContext):
         reply_to_message_id=query_message_id
     )
     proc: subprocess.Process = await create_subprocess_shell(
-        f"git pull main; git checkout main~{downgrade_amount}",
+        f"git pull origin main; git checkout main~{downgrade_amount}",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
